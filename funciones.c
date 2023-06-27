2e64d2f53759226a0425f06853f20ef1ed098792
matein2202 <130107080+matein2202@users.noreply.github.com>
2023-06-27 14:11:49 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "funciones.h"
 
-void mostrarMenuCategoria(char categoria[componentes_maximos][nom], float precios[componentes_maximos], int n) {
+void mostrarMenuCategoria(char categoria[][50], float precios[], int n) {
     printf("***************************\n");
     for (int i = 0; i < n; i++) {
         printf("%d. %s - $%.2f\n", i+1, categoria[i], precios[i]);
@@ -9,43 +9,36 @@ void mostrarMenuCategoria(char categoria[componentes_maximos][nom], float precio
     printf("***************************\n");
 }
 
-void comprar(char categoria[componentes_maximos][nom], float precios[componentes_maximos], int n, float* total) {
+void comprar(char categoria[][50], float precios[], int n, float* total) {
     int opcion;
     printf("Ingrese el numero del componente que desea comprar (0 para cancelar): ");
     scanf("%d", &opcion);
 
-    if (opcion < 0 || opcion > n) {
+    if (opcion < 1 || opcion > n) {
         printf("Opcion invalida.\n");
         return;
     }
 
-    if (opcion == 0) {
-        printf("Compra cancelada.\n");
-        return;
-    }
-
-    printf("Has seleccionado: %s - $%.2f\n", categoria[opcion-1], precios[opcion-1]);
-    *total += precios[opcion-1];
+    float precioSeleccionado = precios[opcion-1];
+    printf("Has seleccionado: %s - $%.2f\n", categoria[opcion-1], precioSeleccionado);
+    *total += precioSeleccionado;
     printf("Compra realizada con exito.\n");
 }
 
-void eliminar(char categoria[componentes_maximos][nom], float precios[componentes_maximos], int n, float* total) {
+void eliminar(char categoria[][50], float precios[], int n, float* total) {
     int opcion;
     printf("Ingrese el numero del componente que desea eliminar (0 para cancelar): ");
     scanf("%d", &opcion);
 
-    if (opcion < 0 || opcion > n) {
+    if (opcion < 1 || opcion > n) {
         printf("Opcion invalida.\n");
         return;
     }
 
-    if (opcion == 0) {
-        printf("Eliminacion cancelada.\n");
-        return;
-    }
-
-    printf("Has eliminado: %s - $%.2f\n", categoria[opcion-1], precios[opcion-1]);
-    *total -= precios[opcion-1];
+    float precioEliminado = precios[opcion-1];
+    printf("Has eliminado: %s - $%.2f\n", categoria[opcion-1], precioEliminado);
+    *total -= precioEliminado;
     printf("Eliminacion realizada con exito.\n");
 }
+
 
