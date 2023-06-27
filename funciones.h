045b9aa377b4c22829148a8c61cc09fdd5c26901
matein2202 <130107080+matein2202@users.noreply.github.com>
2023-06-27 15:02:27 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,8 +1,11 @@
 
+
 void mostrarMenuCategoria(char categoria[][50], float precios[], int n);
-void agregarComponente(char categoria[][50], float precios[], int* n);
+int agregarComponente(char categoria[][50], float precios[], int n);
 void modificarPrecio(char categoria[][50], float precios[], int n);
-void eliminarComponente(char categoria[][50], float precios[], int* n);
+void eliminarComponente(char categoria[][50], float precios[], int *n);
+
+
 
 
 
